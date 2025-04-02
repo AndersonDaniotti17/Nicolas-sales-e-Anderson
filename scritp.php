6fc53fbbf43ad531f7c2ce1436d29228dbb9d445
O ++ <andersondaniotti17@gmail.com>
2025-04-02 15:09:48 -0300

    Add files via upload

---
+++ scritp.php
@@ -0,0 +1,21 @@
+<div style="color: blue; font-size: 14px; text-align: center">
+<?php
+//variáveis
+
+$nome = $_GET['nome'];
+$idade = $_GET['idade'];
+$altura = $_GET['altura'];
+$sexo = $_GET['sexo'];
+
+
+echo "Seu nome é ". $nome;
+echo "<br>";
+echo "Sua idade é ". $idade;
+echo "<br>";
+echo "Sua altura é ". $altura;
+echo "<br>";
+echo "Seu sexo é ". $sexo;
+echo "<br>";
+
+?>
+</div>
